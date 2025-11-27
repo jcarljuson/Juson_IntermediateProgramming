c2b17de3511ecd818d8bfde28bfa8f4dbabf6be0
jcarljuson <137998306+jcarljuson@users.noreply.github.com>
2025-11-27 08:58:29 +0800

    Add files via upload

---
+++ Juson_Biography/Main (1).java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args){
+
+        String my_name = "Jcarl C. Juson";
+        int my_age = 18;
+
+        String my_preschool = "Western Antipolo Mission School";
+        String my_primaryschool = "Spectrum Learning Center Inc.";
+        String my_highschool = "Virgin of Carmel High School";
+        String my_tertiaryschool = "National University Manila";
+
+        String my_hobbies = "Robotics, BCI Interface, Hackatons, Piano, Electromyography, Electroencephalogram";
+        String achievement1 = "Nasa International Space Apps Challenge\n \t\t(GLOBAL NOMINEE) (Awaiting for final winner on December)";
+        String achievement2 = "Best in Research & Innovation\n \t\t3D Printed Nerve Controlled Prosthetic Arm \n \t\t\tfor Transradial Amputees (EEG)";
+        String achievement3 = "4x Robotics Competition Champion in Highschool";
+
+        System.out.println("Name: " + my_name);
+        System.out.println("Age: " + my_age);
+        System.out.println("\nEducational Background: " + my_preschool + " - Kinder \n \t\t\t " + my_primaryschool + " - Elementary \n \t\t\t " + my_highschool + " - High School\n \t\t\t " + my_tertiaryschool + " - College (Currently)");
+
+        System.out.println("\nHobbies: " + my_hobbies);
+
+        System.out.println("\nAchievements: -" + achievement1 + "\n \t\t-" + achievement2 + "\n\t\t-" + achievement3);
+    }
+}
---
+++ Juson_Calculator/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 5;
+
+        int addition = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;
+        float average = addition / 2f;
+
+            System.out.println("Addition: " + addition);
+            System.out.println("Subtraction: " + subtraction);
+            System.out.println("Multiplication: " + multiplication);
+            System.out.println("Division: " + division);
+            System.out.println("Average: " + average);
+        }
+    }
